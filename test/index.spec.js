99b29a4cbe9db37138768d1ce0b914d4de4c18f4
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2023-07-10 11:08:15 +0300

    Add test: with --suppress-ok and --suppress-todo

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,11 +4,21 @@ const fs = require('node:fs');
 const { assert } = require('chai');
 
 describe('Calling this on itself', () => {
-  it('should generate expected report', () => {
+  it('should generate full expected report', () => {
     // given
-    const expected = JSON.parse(fs.readFileSync('./test/expected-report.json'));
+    const expected = JSON.parse(fs.readFileSync('./test/full.expected-report.json'));
     // when
     const actual = JSON.parse(execSync('node ./src/index.js'));
+
+    // then
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should generate minimal expected report', () => {
+    // given
+    const expected = JSON.parse(fs.readFileSync('./test/minimal.expected-report.json'));
+    // when
+    const actual = JSON.parse(execSync('node ./src/index.js --suppress-todo --suppress-ok'));
 
     // then
     assert.deepEqual(actual, expected);
